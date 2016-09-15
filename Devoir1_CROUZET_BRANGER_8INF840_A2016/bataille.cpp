d5a6a9e861aef929c7ab21dab94b85ae4ed55e68
camboui <mathias.branger@gmail.com>
2016-09-14 20:08:15 -0400

    mise à jour du main selon Carte

--- Devoir1_CROUZET_BRANGER_8INF840_A2016/bataille.cpp
+++ Devoir1_CROUZET_BRANGER_8INF840_A2016/bataille.cpp
@@ -1,13 +1,13 @@
 // Devoir1_CROUZET_BRANGER_8INF840_A2016.cpp : Defines the entry point for the console application.
-//
-
+#include <cstdlib>
+#include <iostream>
 
 #include "Pile.h"
 #include "Carte.h"
 #include "Joueur.h"
-#include <cstdlib>
-#include <iostream>
 
+
+using namespace std;
 
 int entier_alea(int min, int max)
 {
@@ -18,10 +18,7 @@ void remplir_pile(Pile<Carte> & p)
 {
 	for (int i = 0; i < 100;i++)
 	{
-		Carte c = new Carte();
-		//c.couleur = entier_alea(0,1);
-		//c.val = entier_alea(1,10);
-		//c.bonus = entier_alea(1,4);
+		Carte c = Carte();
 		p.empiler(c);
 	}
 
@@ -45,7 +42,77 @@ void distribuer_joueurs(int nb_cartes, Pile<Carte> p,Joueur j1,Joueur j2)
 
 void jouer_un_tour(Joueur j1, Joueur j2)
 {
+	Carte c1 = j1.pile.depiler();
+	Carte c2 = j2.pile.depiler();
 
+	if (c1.getValeur()<c2.getValeur())
+	{
+		j2.pileFinale.empiler(c1);
+		j2.pileFinale.empiler(c2);
+	}
+	else if (c1.getValeur()>c2.getValeur())
+	{
+		j1.pileFinale.empiler(c1);
+		j1.pileFinale.empiler(c2);
+	}
+	else
+	{
+		if (c1.getCouleur() == Couleur::ROUGE && c2.getCouleur() == Couleur::NOIR)
+		{
+			j1.pileFinale.empiler(c1);
+			j1.pileFinale.empiler(c2);
+		}
+		else if (c2.getCouleur() == Couleur::ROUGE && c1.getCouleur() == Couleur::ROUGE)
+		{
+			j2.pileFinale.empiler(c1);
+			j2.pileFinale.empiler(c2);
+		}
+		else
+		{
+			j1.pileFinale.empiler(c1);
+			j2.pileFinale.empiler(c2);
+		}
+	}
+}
+
+float compter_points (Joueur j)
+{
+	float compte = 0.;
+	while(!j.pileFinale.estVide())
+	{
+		Carte c = j.pileFinale.depiler();
+		if(c.getCouleur() == Couleur::ROUGE)
+		{
+			compte += 1.5*c.getBonus() * c.getValeur();
+		}
+		else
+		{
+			compte += c.getBonus()* c.getValeur();
+		}
+	}
+	return compte;
+}
+
+void afficher_score(Joueur j1, Joueur j2)
+{
+	float score1, score2;
+	score1 = compter_points(j1);
+	score2 = compter_points(j2);
+	cout << "Résultats de la partie : " << endl;
+	cout << "Joueur 1 : " << score1 << endl;
+	cout << "Joueur 2 : " << score1 << endl;
+	if(score1<score2)
+	{
+		cout << "Le joueur 1 a gagné " << endl;
+	}
+	else if (score1>score2)
+	{
+		cout << "Le joueur 2 a gagné " << endl;
+	}
+	else
+	{
+		cout << "Egalité" << endl;
+	}
 }
 
 int main(void)
@@ -62,7 +129,7 @@ int main(void)
 	distribuer_joueurs(50, pioche, j1, j2);
 
 	//Boucle de jeu
-	while(!j1.tas.estVide() && !j1.tas.estVide())
+	while(!j1.pile.estVide() && !j1.pile.estVide())
 	{
 		jouer_un_tour(j1,j2);
 	}
